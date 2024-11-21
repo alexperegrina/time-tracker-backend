47499d4db4e16e1050855c2df31862d8fe75197e
Alex Peregrina <alexperegrina@gmail.com>
2024-11-21 20:17:36 +0100

    Add Doctrine and configure using bundles

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,9 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+
     DegustaBox\Core\CoreBundle::class => ['all' => true],
     DegustaBox\Auth\AuthBundle::class => ['all' => true],
     DegustaBox\TimeRecording\TimeRecordingBundle::class => ['all' => true],
