02b0f7e57ae6b09ce387a8e6654d4891e0c7a0f6
Alex Peregrina <alexperegrina@gmail.com>
2024-11-25 21:20:53 +0100

    Add schema validation in request from endpoint registration

--- src/Auth/src/Interfaces/Controller/UserController.php
+++ src/Auth/src/Interfaces/Controller/UserController.php
@@ -7,6 +7,7 @@ use DegustaBox\Auth\Application\Command\CreateUser\CreateUserCommand;
 use DegustaBox\Auth\Application\Command\SetRoles\SetRolesCommand;
 use DegustaBox\Auth\Application\Command\VerifyUser\VerifyUserCommand;
 use DegustaBox\Core\Domain\Messenger\Bus\CommandBus;
+use DegustaBox\Core\Domain\Validator\SchemaValidator;
 use DegustaBox\Core\Domain\ValueObject\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,12 +16,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    public function __construct(private readonly CommandBus $commandBus) {}
+    public function __construct(
+        private readonly CommandBus $commandBus,
+        private readonly SchemaValidator $schemaValidator
+    ) {}
 
     #[Route('/registration', name: 'auth_user_registration', methods: ['POST'])]
     public function registration(Request $request): Response
     {
         $data = json_decode($request->getContent(), true, 512, JSON_THROW_ON_ERROR);
+        $data = $this->schemaValidator->validate(
+            $data,
+            '@AuthBundle/Resources/schema/Interfaces/Controller/UserController/Request/registration.json'
+        );
 
         $id = Uuid::uuid4();
         $command = new CreateUserCommand($id->value, $data['email'], $data['password']);
